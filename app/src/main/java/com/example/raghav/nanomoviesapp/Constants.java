6ab94cff8dae0126993fc6e36c1e28dda5c1242b
Raghav Malik <raghav1987@gmail.com>
2015-07-19 19:03:27 +0530

    add constants file

---
+++ app/src/main/java/com/example/raghav/nanomoviesapp/Constants.java
@@ -0,0 +1,9 @@
+package com.example.raghav.nanomoviesapp;
+
+/**
+ * Created by raghav on 7/18/15.
+ */
+public final class Constants {
+    public static final String API_KEY="0afa7d3d93c6b0544e3f2a01fc31c0dd";
+    public static final String DETAIL_ACTIVITY_INTENT="CURRENT_MOVIE";
+}
